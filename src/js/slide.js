0d2bd99d712a1a17f3b3a58a8a791a906a21eda2
Adam Rutter <46485756+adamrutter@users.noreply.github.com>
2019-11-28 16:27:33 +0000

    Accessibility (#14)
    * Added title, role=img, aria-labelledby to svgs
    
    * Added aria-label to logo link
    
    * Added title, desc, role=button, aria-pressed, relevant js
    
    * Added label for project image figure
    
    * Added aria-hidden to device frames
    
    * Added aria-hidden to use elements
    
    * Moved svg out of h1
    
    * Added title, role=img, aria-labelledby to emoji
    
    * Added aria-hidden to use element
    
    * Added missing ids
    
    * Fixed duplicate ids
    
    * Added aria-label to figure
    
    * Added aria-hidden to hidden form fields
    
    * Removed aria-label
    
    * Added aria-hidden to inactive links
    
    * Changed inactive links to an anchor
    
    * Conditionally removed figcaption, and added aria-label in that case
    
    * Improved flow of accordian section in aria
    
    * Shortened aria-label
    
    * Improved usability with no css

--- src/js/slide.js
+++ src/js/slide.js
@@ -4,9 +4,11 @@ function slide(element) {
 	if (element.classList.contains('hidden')) {
 		element.classList.replace('hidden', 'visible');
 		element.style.height = element.scrollHeight + 'px';
+		element.setAttribute('aria-hidden', 'false');
 		// If the element is visible...
 	} else if (element.classList.contains('visible')) {
 		element.classList.replace('visible', 'hidden');
 		element.style.height = 0 + 'px';
+		element.setAttribute('aria-hidden', 'true');
 	}
 }
