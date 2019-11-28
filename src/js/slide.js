953b4421ea3980aa3768b231839a76bf7cdebdf5
adamrutter <46485756+adamrutter@users.noreply.github.com>
2019-11-28 19:45:27 +0000

    Moved styling to css

--- src/js/slide.js
+++ src/js/slide.js
@@ -3,12 +3,10 @@ function slide(element) {
 	// If the element is hidden...
 	if (element.classList.contains('hidden')) {
 		element.classList.replace('hidden', 'visible');
-		element.style.height = element.scrollHeight + 'px';
 		element.setAttribute('aria-hidden', 'false');
 		// If the element is visible...
 	} else if (element.classList.contains('visible')) {
 		element.classList.replace('visible', 'hidden');
-		element.style.height = 0 + 'px';
 		element.setAttribute('aria-hidden', 'true');
 	}
 }
